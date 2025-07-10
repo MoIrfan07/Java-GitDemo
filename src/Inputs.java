983428ab845f73b7b4ff4eab2afd3352a0fb7823
Mohammad Irfan <Mohammad.Irfan@novigo.com>
2025-07-10 16:18:33 +0530

    New files

---
+++ src/Inputs.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Inputs {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        //Int Inputs
+//        System.out.print("Please provide the roll no. : ");
+//        int n = inp.nextInt();
+//        System.out.println("Your Roll no. is : " +n);
+
+        //Examples
+//        int n = 230_500_000;
+//        System.out.println(n);
+
+//        float n = inp.nextFloat();
+//        System.out.println(n);
+
+//        float n = inp.nextFloat();
+//        float m = inp.nextFloat();
+//        float sum = n + m;
+//        System.out.println(sum);
+
+        //Type Casting
+//        int n = (int)(56.56f);
+//        System.out.println(n);
+
+        //automatic type promotion
+        //Ex 1
+//        int a = 259;
+//        byte b = (byte)(a);
+//        System.out.println(b);
+        //Ex 2
+//        byte a = 50;
+//        byte b = 40;
+//        byte c = 100;
+//        int d = a * b / c;
+//        System.out.println(d);
+
+
+    }
+}
