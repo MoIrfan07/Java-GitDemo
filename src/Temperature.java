4de75e2228faf9396d1053e024a0c3a78c8b9204
Mohammad Irfan <Mohammad.Irfan@novigo.com>
2025-07-10 16:22:05 +0530

    New files

--- src/Temperature.java
+++ src/Temperature.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Temperature {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
+        System.out.println("Please enter temperature in C : ");
         float c = inp.nextFloat();
 
         float f = (c * 9/5) + 32;
