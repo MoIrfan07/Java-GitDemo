9f5c9b49383c534140ca1e17e1891c60b982d237
Mohammad Irfan <Mohammad.Irfan@novigo.com>
2025-07-10 15:38:53 +0530

    New file

---
+++ src/Primitives.java
@@ -0,0 +1,10 @@
+public class Primitives {
+    public static void main(String[] args) {
+        int num = 555;
+        char str = 'I';
+        float flt = 67.56F;
+        double LargeDecimalNums = 43243.456;
+        long LargeIntegers = 7647456465465L;
+        boolean check = false;
+    }
+}
